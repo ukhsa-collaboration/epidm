47684d462b0f5487a00992ee022d20ceb71cad0e
fsloots <frederick.sloots@ukhsa.gov.uk>
2025-11-26 17:57:31 +0000

    Adding expect_warning to test-valid_nhs.R tests to prevent warnings displaying when runnning tests

--- tests/testthat/test-valid_nhs.R
+++ tests/testthat/test-valid_nhs.R
@@ -11,18 +11,17 @@ test_that("invalid NHS number fails checksum", {
 })
 # -----------------------------------------------------------
 test_that("NHS number with wrong length is invalid", {
-  expect_equal(valid_nhs(123456789), 0)   # 9 digits
-  expect_equal(valid_nhs(12345678901), 0) # 11 digits
+  expect_warning(expect_equal(valid_nhs(123456789), 0))   # 9 digits
+  expect_warning(expect_equal(valid_nhs(12345678901), 0)) # 11 digits
 })
 # -----------------------------------------------------------
 test_that("NA input returns 0", {
-  expect_equal(valid_nhs(NA), 0)
+  expect_warning(expect_equal(valid_nhs(NA), 0))
 })
 # -----------------------------------------------------------
 test_that("vector input returns vector of results", {
   nums <- c(9434765919, 9434765918, 1111111111, NA)
-  result <- valid_nhs(nums)
-  expect_equal(result, c(1, 0, 0, 0))
+  expect_warning(expect_equal(valid_nhs(nums), c(1, 0, 0, 0)))
 })
 # -----------------------------------------------------------
 
